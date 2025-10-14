9dc62cdeaef48fbbe10fd6569aab4ee58136d2a7
yangjinbo <yangjinbo@qding.me>
2025-10-14 10:27:57 +0800

    update terrform

---
+++ .github/terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source = "aliyun/alicloud"
+      version = "= 1.247.0"
+    }
+  }
+}
+
+provider "alicloud" {
+  region = var.region
+}
+
+resource "alicloud_vpc" "main" {
+  vpc_name    = var.vpc_name
+  cidr_block  = var.vpc_cidr
+  description = var.vpc_description
+}
+
+# 输出信息
+output "vpc_id" {
+  value = alicloud_vpc.main.id
+}
+
+output "vpc_name" {
+  value = alicloud_vpc.main.vpc_name
+}
+
+output "cidr_block" {
+  value = alicloud_vpc.main.cidr_block
+}
