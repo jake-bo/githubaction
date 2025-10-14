2463527b62d95211d664301da779d60efb049fd3
yangjinbo <yangjinbo@qding.me>
2025-10-14 17:14:14 +0800

    update main.tf

--- .github/terraform/variables.tf
+++ .github/terraform/variables.tf
@@ -1,19 +1,19 @@
-variable "vpc_name" {
-  description = "The name of the VPC"
-  type        = string
-}
+# variable "vpc_name" {
+#   description = "The name of the VPC"
+#   type        = string
+# }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "172.16.0.0/16"
-}
+# variable "vpc_cidr" {
+#   description = "CIDR block for the VPC"
+#   type        = string
+#   default     = "172.16.0.0/16"
+# }
 
-variable "vpc_description" {
-  description = "Description for the VPC"
-  type        = string
-  default     = "Created by GitHub Actions Terraform"
-}
+# variable "vpc_description" {
+#   description = "Description for the VPC"
+#   type        = string
+#   default     = "Created by GitHub Actions Terraform"
+# }
 
 variable "region" {
   description = "Alibaba Cloud region"
